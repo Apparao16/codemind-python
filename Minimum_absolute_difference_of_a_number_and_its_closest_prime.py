1e51bbefc20c35c677ddbd69f6b485438f084679
Apparao16 <102518653+Apparao16@users.noreply.github.com>
2022-06-04 01:11:09 +0530

    a = int(input()) def p(a): 	b= 0 	for i in range (2,int(a**0.5+1)) : 		if a % i == 0 : 			b +=1 	return(b<1and a != 1 ) b = list(filter(p,range(1,a*2))) c = [abs(a-i) for i in b] print(min(c))

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,10 @@
+a = int(input())
+def p(a):
+	b= 0
+	for i in range (2,int(a**0.5+1)) :
+		if a % i == 0 :
+			b +=1
+	return(b<1and a != 1 )
+b = list(filter(p,range(1,a*2)))
+c = [abs(a-i) for i in b]
+print(min(c))
